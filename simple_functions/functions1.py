eca9ef32fb3b4d9c0a1fb4688107635b535f0f08
acse-owk24 <oscar.katzinski24@imperial.ac.uk>
2024-10-15 12:00:04 +0100

    factorial

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,5 +1,6 @@
 
-__all__ = ['my_sum']
+from functools import cache
+__all__ = ['my_sum', 'factorial']
 
 
 def my_sum(iterable):
@@ -7,3 +8,8 @@ def my_sum(iterable):
     for i in iterable:
         tot += i*2
     return tot
+
+@cache
+def factorial(n):
+    return n * factorial(n-1) if n else 1
+
